bb8cc400fa008d6651fa7e1339f03180b8a1c170
ZoRadinski <112024493+ZoRadinski@users.noreply.github.com>
2022-09-14 13:43:23 -0400

    added assets

---
+++ My project/Assets/scripts/rectanglesomething.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rectanglesomething : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello world");
+        Debug.Log(this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Frame");
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (speed * Time.deltaTime));
+        //Debug.Log(speed * Time.deltaTime);
+    }
+}
